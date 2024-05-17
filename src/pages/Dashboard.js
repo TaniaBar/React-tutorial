24460f4da65ebb8c9ddd6dd31369f3456517caff
TaniaBar <taniabarillaro1@gmail.com>
2024-05-17 12:12:33 +0200

    I introduced parameter in dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -9,7 +9,7 @@
 
 // export default Dashboard;
 
-import { Routes, Link, Route } from "react-router-dom";
+import { Routes, Link, Route, useParams } from "react-router-dom";
 
 const Dashboard = () => {
   return (
@@ -28,7 +28,7 @@ const Dashboard = () => {
       <div className="dashboard">
         <Routes>
           <Route path="/" element={<Profile />}></Route>
-          <Route path="orders" element={<Orders />}></Route>
+          <Route path="orders/*" element={<Orders />}></Route>
           <Route path="quotes" element={<Quotes />}></Route>
         </Routes>
       </div>
@@ -36,24 +36,35 @@ const Dashboard = () => {
   );
 };
 
-
 export const Profile = () => {
   return <h2>Profile</h2>;
 };
 
 export const Orders = () => {
-    const orderIds = ["10001", "10002", "10003"];
-    return (
-      <>
-        <h2>Orders</h2>
-        <ul>
-          {orderIds.map((orderId) => (
-            <li>Voir commande #{orderId}</li>
-          ))}
-        </ul>
-      </>
-    );
-  };
+  const orderIds = ["10001", "10002", "10003"];
+  return (
+    <>
+      <h2>Orders</h2>
+      <ul>
+        {orderIds.map((orderId) => (
+          <li key={orderId}>
+            <Link to={`orderdetails/${orderId}`}>Voir commande #{orderId}</Link>
+            {/* Link ai dettagli dell'ordine */}
+          </li>
+        ))}
+      </ul>
+      <Routes>
+        <Route path="orderdetails/:orderId" element={<OrderDetails />} />
+      </Routes>
+   
+    </>
+  );
+};
+
+export const OrderDetails = () => {
+  const {orderId} = useParams();
+  return <h2>Details of order {orderId}</h2>;
+};
 
 export const Quotes = () => {
   return <h2>Quotes</h2>;
